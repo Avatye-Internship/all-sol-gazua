a8433302e6478d27fdb55327606b29f7cb364eca
ngmin27 <ngmin27@avatye.com>
2023-07-31 10:03:00 +0900

    [complete]

--- Week/7.W3/백준_로프_kyungmin.py
+++ Week/7.W3/백준_로프_kyungmin.py
@@ -4,10 +4,10 @@ n = int(input())
 k = []
 for _ in range(n):
     k.append(int(input()))
-k.sort()
+k.sort() #정렬
 
 answers = []
-for x in k:
-    answers.append(x*n)
-    n -= 1
-print(max(answers))
+for x in k: 
+    answers.append(x*n) #제일 작은 로프부터 최대값 넣기
+    n -= 1 
+print(max(answers)) # 제일 큰 값
